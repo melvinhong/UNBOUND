2ee15dbaf11dcd9820bfe15f5db784b6fdc61bbd
Melvin <melvin1hong@gmail.com>
2020-11-12 03:29:36 +1100

    update lpages to singes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Enable the asset pipeline
+  config.assets.enabled = true
+  config.assets.paths << Rails.root.join('/app/assets/fonts')
 end
